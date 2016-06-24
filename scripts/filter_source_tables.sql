1cd746089af3e1a616229180031d902ac7ae8cbd
Maxim Moinat <maxim@thehyve.nl>
2016-06-24 13:31:33 +0200

    Added filter on missing age

--- scripts/filter_source_tables.sql
+++ scripts/filter_source_tables.sql
@@ -2,31 +2,45 @@
 
 -- Delete rows with dates that are not in the format yyyymmdd or empty
 -- e.g. '.', 20030, '   1', '2011-02-' and '2012' are deleted
+-- Delete rows without an age
 DELETE FROM bayer.patient_sluten
-WHERE indatuma  !~ '\d{8}' OR utdatuma !~ '\d{8}'OR indatuma IS NULL OR utdatuma IS NULL
+WHERE indatuma  !~ '\d{8}' OR utdatuma !~ '\d{8}'
+   OR indatuma IS NULL     OR utdatuma IS NULL
+   OR alder IS NULL
 ;
 
 DELETE FROM bayer.patient_oppen
-WHERE indatuma  !~ '\d{8}' OR indatuma IS NULL
+WHERE indatuma  !~ '\d{8}'
+   OR indatuma IS NULL
+   OR alder IS NULL
 ;
 
 DELETE FROM bayer.patient_dag_kiru
-WHERE indatuma  !~ '\d{8}' OR indatuma IS NULL
+WHERE indatuma  !~ '\d{8}'
+   OR indatuma IS NULL
+   OR alder IS NULL
 ;
 
 DELETE FROM bayer.patient_sluten_long
-WHERE indatuma  !~ '\d{8}' OR utdatuma !~ '\d{8}' OR indatuma IS NULL OR utdatuma IS NULL
+WHERE indatuma  !~ '\d{8}' OR utdatuma !~ '\d{8}'
+   OR indatuma IS NULL OR utdatuma IS NULL
+   OR alder IS NULL
 ;
 
 DELETE FROM bayer.patient_oppen_long
-WHERE indatuma  !~ '\d{8}' OR indatuma IS NULL
+WHERE indatuma  !~ '\d{8}'
+   OR indatuma IS NULL
+   OR alder IS NULL
 ;
 
 DELETE FROM bayer.patient_dag_kiru_long
-WHERE indatuma  !~ '\d{8}' OR indatuma IS NULL
+WHERE indatuma  !~ '\d{8}'
+   OR indatuma IS NULL
+   OR alder IS NULL
 ;
 
 -- With drugs this problem not yet arises.
--- DELETE FROM bayer.drug
--- WHERE edatum  !~ '\d{1,2}/\d{1,2}/\d{4}'
--- ;
+DELETE FROM bayer.drug
+WHERE edatum  !~ '\d{1,2}/\d{1,2}/\d{4}'
+   OR alder IS NULL
+;
