f747fa444d3aec0e1c91fbd5cecc771f6f4a7b7e
Maxim Moinat <maxim@thehyve.nl>
2017-01-18 10:53:42 +0100

    Updated move achilles script to new VM folder structure

--- run_achilles/move_achilles_json.sh
+++ run_achilles/move_achilles_json.sh
@@ -1,4 +1,4 @@
 # Copy the Achilles data to the Achilles folder
-cp -r /media/sf_etl_vm/achilles_data/* /opt/ohdsi_docroot/achilles/data/
+cp -r /media/sf_ohdsi-Atlas-20161013_1/etl_vm/achilles_data/* /opt/ohdsi_docroot/achilles/data/
 # Assign everyone read rights to the data folder. Recursively
 chmod -R 755 /opt/ohdsi_docroot/achilles/data/*
