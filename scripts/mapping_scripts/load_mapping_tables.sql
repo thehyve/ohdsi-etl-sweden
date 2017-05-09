11feda2d5f40c0c7fec00bd97fabcf9d127a9c06
Maxim Moinat <maxim@thehyve.nl>
2017-05-09 16:44:39 +0200

    Separated mapping tables and resource tables

--- scripts/mapping_scripts/load_mapping_tables.sql
+++ scripts/mapping_scripts/load_mapping_tables.sql
@@ -59,9 +59,9 @@ CREATE TABLE etl_mappings.unit (
 \copy etl_mappings.unit FROM './mapping_tables/unit.csv'   WITH HEADER CSV
 
 -- Mapping tables directly into cdm
-\copy location (location_id, county) FROM './mapping_tables/lan_locations.csv' WITH HEADER CSV
-\copy care_site ( care_site_id, care_site_source_value, care_site_name, location_id ) FROM './mapping_tables/sjukhus_care_site.csv' WITH HEADER CSV;
-\copy provider (provider_id, provider_name, specialty_concept_id, specialty_source_value) FROM './mapping_tables/spkod_specialty.csv' WITH HEADER CSV
+\copy location (location_id, county) FROM './resources/lan_locations.csv' WITH HEADER CSV
+\copy care_site ( care_site_id, care_site_source_value, care_site_name, location_id ) FROM './resources/sjukhus_care_site.csv' WITH HEADER CSV;
+\copy provider (provider_id, provider_name, specialty_concept_id, specialty_source_value) FROM './resources/spkod_specialty.csv' WITH HEADER CSV
 
 -- Set location_source_value to be the same as the location_id
 UPDATE location
